66d9554a15a1dcc980d5b24724ba7000fec3c4a6
Yarimizu14 <yarimizu.kensei14@gmail.com>
2013-07-11 19:41:25 +0900

    ユーザー作成を可能に

--- model/users.js
+++ model/users.js
@@ -1,38 +1,46 @@
 var db  = require('./database');
 
-/*
- * Model access to api server 
- */
-
-
 // ユーザーデータを扱うオブジェクト
 function UserDB() {};
 
 UserDB.prototype = db.createClient();
+
 /*
  *  addUser 
  *  ユーザーを追加する関数 
  */
-UserDB.prototype.addUser = function() {
-    console.log("addUser");
+UserDB.prototype.createUser = function(name, pw, callback) {
+    var q_str = 'INSERT INTO users (name, pw, created) VALUES ("' + name + '", "' + pw + '", now());';
+    this.query(q_str, void 0, function (err, results, fields) {
+        if (err) {
+           throw err; 
+            callback.error();
+        } else {
+            console.log(results);
+            callback(results);
+        };
+    });
 }
 /*
  *  authorizeUser
  *  パスワードを確認する関数
  */
 UserDB.prototype.authorizeUser = function(name, pw, callback) {
-    console.log("authorizeUser");
-    var q_str = 'SELECT * FROM users where name="' + name + '";';
+    var q_str = 'SELECT * FROM users WHERE name="' + name + '";';
     this.query(q_str, void 0, function (err, results, fields) {
-        if(results[0] !==  void 0 && results[0].pw === pw) {
-            console.log("login successful!!");
-            callback.success(results[0]);
-        } else {
-            console.log("login denied!!");
+        if (err) {
+            throw err
             callback.error();
-        };
+        } else {
+            if(results[0] !==  void 0 && results[0].pw === pw) {
+                console.log("login successful!!");
+                callback.success(results[0]);
+            } else {
+                console.log("login denied!!");
+                callback.error();
+            };
+        }
     });
-
 };
 
 function createUserDB() {
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,17 @@
 var userModel   = require('../model/users.js'),
     threadModel = require('../model/thread.js');
 
+
+//モデルをインスタンス化
+var users   = userModel.createUserDB();
+var threads = threadModel.createThreadDB();
+
 /*
  * ログイン画面を表示する
  * GET /login
  */
 exports.referLoginForm = function(req, res) {
-    res.render('user/user_login', { title: 'Login Form'});
+    res.render('user/user_login', { msg_exist : false, msg : "" });
 }
 
 /*
@@ -14,27 +19,38 @@ exports.referLoginForm = function(req, res) {
  * POST /login
  */
 exports.authorize = function(req, res) {
+    var type = req.param('submit');
     var name = req.param('name');
     var pw   = req.param('pw');
 
-    //モデルをインスタンス化
-    var users   = userModel.createUserDB();
-    var threads = threadModel.createThreadDB();
+    var error_callback =  function() {
+        res.render('user/user_login', { msg_exist : true, msg : "Failed, try again" });
+    };
+
 
     //ログイン成功・失敗時のコールバック
-    var callback = {
+    var auth_callback = {
         success : function(user_info) {
-            var data = {};
-            data.user = user_info;
-
             //ユーザー情報を保持
             req.session.user = user_info;
             res.redirect('/thread/list');
         },
-        error : function() {
-            res.render('user/user_login_fail', {});
-        }
+        error : error_callback
     };
-    //認証処理
-    users.authorizeUser(name, pw, callback);
+
+    var create_callback = {
+        success : function(results) {
+            users.authorizeUser(name, pw, auth_callback);
+        },
+        error : error_callback
+    };
+
+    if (type === "regist") {
+        //新規ユーザー作成
+        users.createUser(name, pw, create_callback);
+    } else if (type === "login") {
+        //認証処理
+        users.authorizeUser(name, pw, auth_callback);
+    }
+
 };
