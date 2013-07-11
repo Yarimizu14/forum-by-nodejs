c24023689950ad21045a4641eacf11be8a533355
Yarimizu14 <yarimizu.kensei14@gmail.com>
2013-07-11 17:09:53 +0900

    スレッド一覧のデザイン完成

--- public/javascripts/thread/thread.js
+++ public/javascripts/thread/thread.js
@@ -2,21 +2,21 @@
 
 $(function() {
 
-    $(".resList_each > .resList_del").on("click", function(evt) {
-        var $this = $(this);
-        var self  = $this.parent().attr("data-self");
-        var $this = $(this);
+    $(".resList_each .res_sub_del").on("click", function(evt) {
+        var $this  = $(this);
+        var holder = $this.closest(".resList_each");
+        var self   = holder.attr("data-self");
 
         if (self === "true") {
             $.ajax({
                 type : 'DELETE',
                 url  : '/thread/delete_res_ajax',
                 data : {
-                    res_id : $this.parent().data("id")
+                    res_id : holder.data("id")
                 }
             })
             .done(function(data) {
-                $this.parent().hide();
+                holder.fadeOut("slow");
                 console.log(data);
             })
             .fail(function() {alert("failed");});
@@ -25,53 +25,52 @@ $(function() {
         }
     });
 
-    $(".resList_each > .resList_favorite").on("click", function(evt) {
-        var $this = $(this);
-        var fav   = $this.parent().attr("data-favorite");
-        var options = {};
-
-        if (fav === "false") {
-            options = {
-                type : 'POST',
-                url  : '/thread/create_favorite_ajax',
+    $(".resList_each .img_favorite").on("click", function(evt) {
+        var $this  = $(this);
+        var holder = $this.closest(".resList_each");
+        var fav    = holder.attr("data-favorite");
+        var options = {
                 data : {
                     thread_id : $("#thread_id").attr("value"),
-                    res_id    : $this.parent().data("id")
+                    res_id    : holder.data("id")
                 }
-            }
+        };
+
+        if (fav === "false") {
+            $.extend(options, {
+                type : 'POST',
+                url  : '/thread/create_favorite_ajax',
+            });
             $.ajax(options)
             .done(function(data) {
-                $this.parent().attr("data-favorite", true);
-                var num_elem = $this.parent().find(".resList_favoriteNum");
+                holder.attr("data-favorite", true);
+                $this.attr("src", "/images/9_heart_fill.png");
+                var num_elem = holder.find(".res_sub_favorite_num");
                 var num = +(num_elem.text());
-                num_elem.text((++num).toString());
+                num_elem.text((++num) + '');
             })
             .fail(function() {
                 alert("failed");
             });
 
         } else {
-            options = {
+            $.extend(options, {
                 type : 'DELETE',
                 url  : '/thread/delete_favorite_ajax',
-                data : {
-                    thread_id : $("#thread_id").attr("value"),
-                    res_id    : $this.parent().data("id")
-                }
-            }
+            });
             $.ajax(options)
             .done(function(data) {
-                $this.parent().attr("data-favorite", false);
-                var num_elem = $this.parent().find(".resList_favoriteNum");
+                holder.attr("data-favorite", false);
+                $this.attr("src", "/images/8_heart_stroke.png");
+                var num_elem = holder.find(".res_sub_favorite_num");
                 var num = +(num_elem.text());
-                num_elem.text((--num).toString());
+                num_elem.text((--num) + '');
             })
             .fail(function() {
                 alert("failed");
             });
         };
     });
-
 
 });
 
